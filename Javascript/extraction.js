4b7f3e1af96340cbaf5c45d8d1dd024b35db2ea0
Tariq_Sulley_official <32623579+tariqsulley@users.noreply.github.com>
2020-06-28 12:55:33 +0000

    Initial Commit

---
+++ Javascript/extraction.js
@@ -0,0 +1,14 @@
+    function extractEmails(text)
+    {
+        return text.match(/([a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9._-]+)/gi);
+    }
+
+    function extract()
+    {
+        emails = document.getElementById("txt1").value;
+        extracted_emails = extractEmails(emails);
+        document.getElementById("txt2").value = extracted_emails;
+    }
+    function reset(){
+        document.getElementById("txt2").value = '';
+    }
